59ad3531378e32f92e8d1dd9ce4996a8ead10d52
shahnCM <mahir.fine@hotmail.com>
2020-12-09 14:58:01 +0600

    authentication scaffolding done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\User;
 
 class CreateUsersTable extends Migration
 {
@@ -18,14 +17,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->string('role')->default(User::ROLE_GENERAL_USER);
-            $table->boolean('is_banned')->default(User::BANNED_FLAG_DOWN);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
